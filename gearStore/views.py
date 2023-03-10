953b09da02c65f6960f2c4e5b92c07f068613b05
Alex Deverill <alexdeverill35@gmail.com>
2023-03-10 13:26:08 +0000

    added index and view_category views

--- gearStore/views.py
+++ gearStore/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from models import UserProfile, Category, Gear
 
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': 'This is the Blod Message'}
-    return render(request, 'gearStore/index.html', context=context_dict)
+    context_dict = {}
+    context_dict['boldmessage'] = 'This is the Blod Message'
+    if request.user.is_authenticated():
+        context_dict["user_profile"] = UserProfile.objects.get(user = request.user)
+    else:
+        context_dict["user_profile"] = None
+    context_dict["categories"] = Category.objects.all()
+    return render(request, 'gearStore/index.html', context_dict)
+
+def view_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug = category_name_slug)
+    except Category.DoesNotExist:
+        #Redirect to home page if category doesn't exist
+        return redirect(reverse("gearStore:index"))
+    context_dict["category"] = category
+    context_dict["gear_list"] = Gear.object.filter(category = category)
+    return render(request, 'gearStore/view_category.html', context_dict)
 
 def about(request):
     return render(request, 'gearStore/about.html')
