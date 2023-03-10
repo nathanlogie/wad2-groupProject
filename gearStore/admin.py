6f071e34122fcdc7c647903abeb2a4ce7fdd7464
Nathan Logie <2658532l@student.gla.ac.uk>
2023-03-10 16:57:38 +0000

    Added gear model

--- gearStore/admin.py
+++ gearStore/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from gearStore.models import Category, UserProfile
-# Register your models here.
+from gearStore.models import Category, UserProfile, Gear
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'dateAdded', 'picture')
+
+
+class GearAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category', 'colour', 'size', 'dateAdded', 'picture')
+
 
 admin.site.register(UserProfile)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Gear, GearAdmin)
--- gearStore/models.py
+++ gearStore/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 import django.utils.timezone
 from django.db import models
 from django.contrib.auth.models import User
@@ -31,3 +29,34 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+COLOUR_CHOICES = (
+    ('green', 'GREEN'),
+    ('blue', 'BLUE'),
+    ('red', 'RED'),
+    ('orange', 'ORANGE'),
+    ('black', 'BLACK'),
+)
+
+
+class Gear(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    dateAdded = models.DateField()
+    picture = models.ImageField(default="gear_images/default.png")
+    colour = models.CharField(max_length=6, choices=COLOUR_CHOICES, default="GREEN")
+    size = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Gear"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Gear, self).save(*args, **kwargs)
+        
