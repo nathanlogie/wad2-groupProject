8b383598f13c42acdcc1533b99c3af5d136c4d21
KinLi0424 <2612965l@student.gla.ac.uk>
2023-03-10 04:44:59 +0000

    added index and about templates

--- groupProject/urls.py
+++ groupProject/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
 from gearStore import views
 
 urlpatterns = [
