85aba5b0b4f383bab20b6ab6bb405f84c25ea223
Alex Deverill <2652969d@student.gla.ac.uk>
2023-03-10 16:19:55 +0000

    added login and registration pages

--- gearStore/urls.py
+++ gearStore/urls.py
@@ -6,4 +6,6 @@ app_name = 'gearStore'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
