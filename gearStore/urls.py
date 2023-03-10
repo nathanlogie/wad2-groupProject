5975b7d72ce126c2bd2f35a9ec3b173ca040f9df
craigrc <cameronrcraig3@gmail.com>
2023-03-10 17:40:03 +0000

    Updated css, added navbar base, fixed urls.py

--- gearStore/urls.py
+++ gearStore/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('contact', views.contact, name='contact')
 ]
