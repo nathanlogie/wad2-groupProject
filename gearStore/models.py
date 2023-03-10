e4537375b3179d03e40a0e9534f60eb04a752229
Nathan Logie <2658532l@student.gla.ac.uk>
2023-03-10 11:47:25 +0000

    Added categories model

--- gearStore/models.py
+++ gearStore/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=128)
+    dateAdded = models.DateTimeField(default=datetime.date.today())
+    picture = models.ImageField(upload_to="category_images")
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
