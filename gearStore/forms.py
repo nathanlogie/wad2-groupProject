e1aa777a540af5c2a89ba0c3674b687a7c2bea93
ZhengchuF <2723134f@student.gla.ac.uk>
2023-03-10 15:02:01 +0000

    add in Forms.py

--- gearStore/forms.py
+++ gearStore/forms.py
@@ -1,2 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget= forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+    #class UserProfileForm(forms.ModelForm):
+        #class Meta
